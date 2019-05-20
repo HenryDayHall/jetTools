0f273029e923730132f6fad41a19c235845fec62
Henry Day-Hall <henrydayhall@protonmail.com>
2019-05-20 15:52:15 +0100

    Adding new tree code (unholyMatrimony)

---
+++ unholyMatrimony/start.py
@@ -0,0 +1,7 @@
+# Jets are formed using applyFastJet.cc
+# this takes in the jets, clusters them and writes to the text form of parentedTree
+# read in the data
+# make the jets into parented trees 
+# there should be a Jet class that is an extention of ParentedTree from nltk
+# use Scikit learn RobustScalar to rescale the data
+
