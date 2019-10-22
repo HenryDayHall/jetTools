e881b801ba29bf7421bb5590b446456353f6325f
Henry Day-Hall <henrydayhall@protonmail.com>
2019-10-22 22:19:08 +0100

    Comaprison function added

---
+++ tree_tagger/CompareClusters.py
@@ -0,0 +1,37 @@
+""" compare two jet clustering techniques """
+from tree_tagger import Components
+import sklearn.metrics
+from matplotlib import pyplot as plt
+import numpy as np
+
+def rand_score(eventWise, jet_name1, jet_name2):
+    # two jets clustered from the same eventWise should have
+    # the same JetInput_SourceIdx, 
+    # if I change this in the future I need to update this function
+    selection1 = getattr(eventWise, jet_name1+"_InputIdx")
+    selection2 = getattr(eventWise, jet_name2+"_InputIdx")
+    num_common_events = min(len(selection1), len(selection2))
+    num_inputs_per_event = Components.apply_array_func(len, eventWise.JetInputs_SourceIdx[:num_common_events])
+    scores = []
+    for event_n, n_inputs in enumerate(num_inputs_per_event):
+        labels1 = -np.ones(n_inputs)
+        for i, jet in enumerate(selection1[event_n]):
+            labels1[jet[jet < n_inputs]] = i
+        assert -1 not in labels1
+        labels2 = -np.ones(n_inputs)
+        for i, jet in enumerate(selection2[event_n]):
+            labels2[jet[jet < n_inputs]] = i
+        score = sklearn.metrics.adjusted_rand_score(labels1, labels2)
+        scores.append(score)
+    return scores
+
+def visulise_scores(scores, jet_name1, jet_name2):
+    plt.hist(scores, bins=40, density=True, histtype='stepfilled')
+    mean = np.mean(scores)
+    std = np.std(scores)
+    plt.vlines([mean - 2*std, mean, mean + 2*std], 0, 1, colors=['orange', 'red', 'orange'])
+    plt.xlabel("Rand score")
+    plt.ylabel("Density")
+    plt.title(f"Similarity of {jet_name1} and {jet_name2} (for {len(scores)} events)")
+
+
