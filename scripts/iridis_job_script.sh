30c123000be6e3e959e10f89c36f71e078c955c2
Henry Day-Hall <henrydayhall@protonmail.com>
2021-01-30 09:08:02 +0000

    job script fro iridis

---
+++ scripts/iridis_job_script.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# Set job requirements
+
+#SBATCH -J abcpy1
+#SBATCH -t 10:00:00
+#SBATCH -o o_%j.%x
+#SBATCH -e e_%j.%x
+#SBATCH -p batch
+#SBATCH --ntasks-per-node=20
+#SBATCH --mail-type=END
+#SBATCH --mail-user=henrydayhall@pm.com
+#============================ PBS part ends =====================
+
+
+
+cd $SLURM_SUBMIT_DIR
+
+export last_journal=""
+#export jet_class="SpectralKMeans"
+echo "running $SLURM_JOB_NAME"
+#mpirun -np 20 ipython3 scripts/mpi_cluster_run.py
+ipython3 run_scan_score.py SpectralFull 12000
+
+#echo "May need to up the duration in mpi_cluster_run.py"
+
+
